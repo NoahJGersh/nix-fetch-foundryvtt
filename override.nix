65f3102521da43a67699d4252682dce62fa9ca1d
Noah Gersh <me@noahger.sh>
2025-11-11 13:01:42 -0800

    fix: extract version from provided parameters, not base pkg

--- override.nix
+++ override.nix
@@ -4,11 +4,13 @@ foundryPkg:
   password ? "",
   passwordFile ? "",
   installDir ? "/etc/foundryvtt",
+  majorVersion ? "13",
+  build ? "350",
   ...
 }:
 foundryPkg.overrideAttrs (old:
   let
-    filename = old.src.name;
+    filename = "${majorVersion}.${build}";
   in
   {
     preUnpack = ''
@@ -31,7 +33,7 @@ foundryPkg.overrideAttrs (old:
       curl -L --cookie /tmp/cookies.txt --cookie-jar /tmp/cookies.txt "https://foundryvtt.com/auth/login/ -H "Content-Type: application/x-www-form-urlencoded" -H "DNT: \"1\"" -H "Upgrade-Insecure-Requests: \"1\"" --data $DATA --referer "https://foundryvtt.com/"
 
       # Download the specified build
-      curl -L --cookie /tmp/cookies.txt "Https://foundryvtt.com/releases/download?build=${old.build}&platform=linux" -o ./${filename}
+      curl -L --cookie /tmp/cookies.txt "Https://foundryvtt.com/releases/download?build=${build}&platform=linux" -o ./${filename}
 
       # Add to store and prevent garbage collection
       STORE_PATH=$(nix-store --add-fixed sha256 ${filename})
